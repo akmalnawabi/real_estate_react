a1acb4f78ae9d34e114f0bcf53df80f2305b8e8d
Akmal Khan <akmalnawabi007@gmail.com>
2025-06-28 16:20:28 +0330

    new

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,30 +1,56 @@
-import React, {useState, useEffect, createContext} from 'react';
-import {housesData} from '../data';
+import React, { useState, useEffect, createContext } from "react";
+import { housesData } from "../data";
 
 export const HouseContext = createContext();
 
-const HouseContextProvider = ({children}) => {
+const HouseContextProvider = ({ children }) => {
   const [houses, setHouses] = useState(housesData);
-  const [country, setCountry] = useState('Location (any)');
+  const [country, setCountry] = useState("Location (any)");
   const [countries, setCountries] = useState([]);
   const [property, setProperty] = useState("Property type (any)");
   const [properties, setProperties] = useState([]);
-  const [price, setPrice] = useState('Price range (any)');
+  const [price, setPrice] = useState("Price range (any)");
   const [loading, setLoading] = useState(false);
 
+  // return all countries
+  useEffect(() => {
+    const allCountries = houses.map((house) => {
+      return house.country;
+    });
+
+    const uniqueContries = ["Location (any)", ...new Set(allCountries)];
+    setCountries(uniqueContries);
+  }, []);
+
+  useEffect(() => {
+    const allProperties = houses.map((house) => {
+      return house.type;
+    });
+
+    const uniqueProperties = ["Location (any)", ...new Set(allProperties)];
+    setProperties(uniqueProperties);
+  }, []);
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
   return (
-    <HouseContext.Provider value={{
-      country,
-      setCountry,
-      countries,
-      property,
-      setProperty,
-      properties,
-      price,
-      setPrice,
-      houses,
-      loading
-    }}>
+    <HouseContext.Provider
+      value={{
+        country,
+        setCountry,
+        countries,
+        property,
+        setProperty,
+        properties,
+        price,
+        setPrice,
+        houses,
+        loading,
+        handleClick
+      }}
+    >
       {children}
     </HouseContext.Provider>
   );
