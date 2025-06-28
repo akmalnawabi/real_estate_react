f8f93020f5007a729d9a9bbf21d7045d603b7800
Akmal Khan <akmalnawabi007@gmail.com>
2025-06-28 16:20:38 +0330

    new

--- src/components/PriceRangeDropdown.js
+++ src/components/PriceRangeDropdown.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import { RiWallet3Line, RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+
+import { Menu } from "@headlessui/react";
+import { HouseContext } from "./HouseContext";
 
 const PriceRangeDropdown = () => {
-  return <div>PriceRangeDropdown</div>;
+  const { price, setPrice } = useContext(HouseContext);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const prices = [
+    {
+      value: "Price Range (any)",
+    },
+    {
+      value: "100000-130000",
+    },
+    {
+      value: "130000-1600000",
+    },
+    {
+      value: "160000-190000",
+    },
+  ];
+
+  return (
+    <Menu as="div" className="dropdown relative">
+      <Menu.Button
+        onClick={() => setIsOpen(!isOpen)}
+        className="dropdown-btn w-full text-left"
+      >
+        <RiWallet3Line className="dropdown-icon-primary" />
+        <div>
+          <div className="text-[15px] font-medium">{price}</div>
+          <div className="text-[13px]">Choose Price Range</div>
+        </div>
+
+        {isOpen ? (
+          <RiArrowUpSLine className="dropdown-icon-secondary" />
+        ) : (
+          <RiArrowDownSLine className="dropdown-icon-secondary" />
+        )}
+      </Menu.Button>
+      <Menu.Items className="dropdown-menu">
+        {prices.map((price, index) => {
+          return (
+            <Menu.Item
+              as="li"
+              className="cursor-pointer hover:text-violet-700 transition"
+              onClick={() => setPrice(price.value)}
+              key={index}
+            >
+              {price.value}
+            </Menu.Item>
+          );
+        })}
+      </Menu.Items>
+    </Menu>
+  );
 };
 
 export default PriceRangeDropdown;
