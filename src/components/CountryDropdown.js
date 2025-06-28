45b678433678f87a12637d76b95416c47b964a66
Akmal Khan <akmalnawabi007@gmail.com>
2025-06-28 16:20:16 +0330

    new

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -10,18 +10,34 @@ const CountryDropdown = () => {
 
   return (
     <Menu as="div" className="dropdown relative">
-      <Menu.Button className="dropdwon-btn w-full text-left">
+      <Menu.Button
+        onClick={() => setIsOpen(!isOpen)}
+        className="dropdown-btn w-full text-left"
+      >
         <RiMapPinLine className="dropdown-icon-primary" />
         <div>
-          <div>{country}</div>
-          <div>Select Your Place</div>
-          {isOpen ? (
-            <RiArrowUpSLine className="dropdown-icon-primary" />
-          ) : (
-            <RiArrowDownSLine className="dropdown-icon-primary" />
-          )}
+          <div className="text-[15px] font-medium">{country}</div>
+          <div className="text-[13px]">Select Your Place</div>
         </div>
+
+          {isOpen ? (
+            <RiArrowUpSLine className="dropdown-icon-secondary" />
+          ) : (
+            <RiArrowDownSLine className="dropdown-icon-secondary" />
+          )}
+          
       </Menu.Button>
+      <Menu.Items className='dropdown-menu'>
+        {countries.map((country, index) => {
+          return (
+            <Menu.Item as='li' className='cursor-pointer hover:text-violet-700 transition'
+            onClick={() => setCountry(country)}
+             key={index}>
+              {country}
+            </Menu.Item>
+          )
+        })}
+      </Menu.Items>
     </Menu>
   );
 };
