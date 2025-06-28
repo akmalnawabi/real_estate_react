6ae322ec6cd0d21ad0a14b216f8c7f3131c97800
Akmal Khan <akmalnawabi007@gmail.com>
2025-06-28 15:02:55 +0330

    new

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+
+import { Menu } from "@headlessui/react";
+import { HouseContext } from "./HouseContext";
 
 const CountryDropdown = () => {
-  return <div>CountryDropdown</div>;
+  const { country, setCountry, countries } = useContext(HouseContext);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Menu as="div" className="dropdown relative">
+      <Menu.Button className="dropdwon-btn w-full text-left">
+        <RiMapPinLine className="dropdown-icon-primary" />
+        <div>
+          <div>{country}</div>
+          <div>Select Your Place</div>
+          {isOpen ? (
+            <RiArrowUpSLine className="dropdown-icon-primary" />
+          ) : (
+            <RiArrowDownSLine className="dropdown-icon-primary" />
+          )}
+        </div>
+      </Menu.Button>
+    </Menu>
+  );
 };
 
 export default CountryDropdown;
