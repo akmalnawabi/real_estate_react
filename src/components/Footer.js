133980d5421a56da9b4aaa3d9291eca5b10e78cf
Akmal Khan <akmalnawabi007@gmail.com>
2025-06-30 11:47:53 +0330

    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer className='bg-violet-600 py-8 text-center text-white'>
+    <div className='container'>
+      Copyright &copy; 2025, Akmal Nawabi
+    </div>
+  </footer>;
 };
 
 export default Footer;
