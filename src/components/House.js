d0418608394f6c05d92dcc3c0c54cab247e3975a
Akmal Khan <akmalnawabi007@gmail.com>
2025-06-30 11:59:27 +0330

    final

--- src/components/House.js
+++ src/components/House.js
@@ -9,7 +9,7 @@ const House = ({ house }) => {
       className="bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full
      max-w-[352px] mx-auto cursor-pointer hover:shadow-2xl transition"
     >
-      <img className="mb-8" src={image} />
+      <img className="mb-8" src={image} alt="image" />
       <div className="mb-4 flex gap-x-2 text-sm">
         <div className="bg-green-500 rounded-full text-white px-3">{type}</div>
         <div className="bg-violet-500 rounded-full text-white px-3">
